61e70080095ab0cf99a8f29861a4e8b15a6fd408
Isaac Kit Hong Ng <isaacngkh@gmail.com>
2017-10-21 11:11:08 -0700

    added boxes

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-class Greeting extends Component {
+export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      <Text>Hello {this.props.name}!</Text>
-    );
-  }
-}
-
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+      // Try setting `flexDirection` to `column`.
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
-}
+};
